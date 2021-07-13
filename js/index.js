e8da3fa13e47c095d5e842ab5c601df1c52afdcb
Sergio Campos <sermarcia@hotmail.com>
2021-07-12 23:32:23 -0300

    Criando função para mudança de cor no menu de navegação ao direcionar o mouse em cima do link

--- js/index.js
+++ js/index.js
@@ -1,3 +1,50 @@
 function boasVindas(){
     alert("Hello!!! My name is Sérgio Campos");
 }
+
+
+/** INICIO */
+document.getElementById("inicio").onmouseover = function() {
+    mouseOverInicio();
+};
+document.getElementById("inicio").onmouseout = function() {
+    mouseOutInicio();
+};
+
+function mouseOverInicio() {
+    document.getElementById("inicio").style.color = "#ff6633";
+};
+function mouseOutInicio() {
+    document.getElementById("inicio").style.color = "white";
+};
+
+/** SERVIÇOS */
+document.getElementById("servicos").onmouseover = function() {
+    mouseOverServicos();
+};
+document.getElementById("servicos").onmouseout = function() {
+    mouseOutServicos();
+};
+
+function mouseOverServicos() {
+    document.getElementById("servicos").style.color = "#ff6633";
+};
+
+function mouseOutServicos() {
+    document.getElementById("servicos").style.color = "white";
+};
+
+/** CONTATO */
+document.getElementById("contato").onmouseover = function() {
+    mouseOverContato();
+};
+document.getElementById("contato").onmouseout = function() {
+    mouseOutContato();
+};
+
+function mouseOverContato() {
+    document.getElementById("contato").style.color = "#ff6633";
+};
+function mouseOutContato() {
+    document.getElementById("contato").style.color = "white";
+};
